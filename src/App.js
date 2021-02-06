c1640d17c42f6e221cffb3d848a923834ab34b0b
albert3rd <atpcodeio@gmail.com>
2021-02-07 00:27:10 +1000

    Fixed Front-End design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import './App.css';
 import MarkdownViewer from './components/MarkdownViewer'
 import {Button} from '@material-ui/core'
+import logo from './img/supermarkdown-logo.png'
+
 function App() {
   const [text, setText] = useState('')
 
@@ -11,25 +13,27 @@ function App() {
 
   return (
     <div className="main">
+      <div className="header">
+        <img src={logo} alt="superMarkdownlogo"/>
+        <p> Click A Button and Begin</p>
+      </div>
 
-      <h1>Welcome to Tools MD</h1>
-      <p> Click A Button and Begin</p>
       {/* Tool Bar */}
       <div className="wrapper">
         <div className="tools">
             <h3>Basic Syntax</h3>
-            <Button type="button" value={"#"} onClick={handleButton} variant="contained" color="primary">Heading</Button>
-            <Button type="button" value={"##"} onClick={handleButton} variant="contained" color="primary">Heading 2</Button>
-            <Button type="button" value={"###"} onClick={handleButton} variant="contained" color="primary">Heading 3</Button>
-            <Button type="button" value={"***Text***"} onClick={handleButton} variant="contained" color="primary">Bold</Button>
-            <Button type="button" value={"*Text*"} onClick={handleButton} variant="contained" color="primary">Italicize</Button>
-            <Button type="button" value={"> \n Note: ReactMarkdown does not support BlockQuotes"} onClick={handleButton} variant="contained" color="primary">BlockQuote</Button>
-            <Button type="button" value={"-\n-\n-"} onClick={handleButton} variant="contained" color="primary">UnorderedList</Button>
-            <Button type="button" value={"*\n*\n*"} onClick={handleButton} variant="contained" color="primary">TripleList</Button>
-            <Button type="button" value={"\`code\`"} onClick={handleButton} variant="contained" color="primary">Code</Button>
-            <Button type="button" value={"---"} onClick={handleButton} variant="contained" color="primary">Horizontal Rule</Button>
-            <Button type="button" value={"[title](https://www.example.com)"} onClick={handleButton} variant="contained" color="primary">Link</Button>
-            <Button type="button" value={"![alt text](image.jpg)\n Please Note: Image will not show up on this doc. Please make a path to the image in your directory."} onClick={handleButton} variant="contained" color="primary">Image</Button>
+            <Button type="button" value={"#"} onClick={handleButton} variant="contained" color="inherit">Heading</Button>
+            <Button type="button" value={"##"} onClick={handleButton} variant="contained" color="inherit">Heading 2</Button>
+            <Button type="button" value={"###"} onClick={handleButton} variant="contained" color="inherit">Heading 3</Button>
+            <Button type="button" value={"***Text***"} onClick={handleButton} variant="contained" color="inherit">Bold</Button>
+            <Button type="button" value={"*Text*"} onClick={handleButton} variant="contained" color="inherit">Italicize</Button>
+            <Button type="button" value={"> \n Note: ReactMarkdown does not support BlockQuotes"} onClick={handleButton} variant="contained" color="inherit">BlockQuote</Button>
+            <Button type="button" value={"-\n-\n-"} onClick={handleButton} variant="contained" color="inherit">Unordered List</Button>
+            <Button type="button" value={"1.\n2.\n3."} onClick={handleButton} variant="contained" color="inherit">Ordered List</Button>
+            <Button type="button" value={"\`code\`"} onClick={handleButton} variant="contained" color="inherit">Code</Button>
+            <Button type="button" value={"---"} onClick={handleButton} variant="contained" color="inherit">Horizontal Rule</Button>
+            <Button type="button" value={"[title](https://www.example.com)"} onClick={handleButton} variant="contained" color="inherit">Link</Button>
+            <Button type="button" value={"![alt text](image.jpg)\n Please Note: Image will not show up on this doc. Please make a path to the image in your directory."} onClick={handleButton} variant="contained" color="inherit">Image</Button>
             <h3>More Features / Coming Soon</h3>
         </div>
         <div className="viewer">
