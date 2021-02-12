2df63c87116a99a9e9fab689642735eba2ee16ad
albert3rd <atpcodeio@gmail.com>
2021-02-13 01:18:20 +1000

    Added Intermediate Buttons

--- src/App.js
+++ src/App.js
@@ -31,11 +31,15 @@ function App() {
             <Button type="button" value={"> \n Note: ReactMarkdown does not support BlockQuotes"} onClick={handleButton} variant="contained" color="inherit">BlockQuote</Button>
             <Button type="button" value={"-\n-\n-"} onClick={handleButton} variant="contained" color="inherit">Unordered List</Button>
             <Button type="button" value={"1.\n 2.\n 3."} onClick={handleButton} variant="contained" color="inherit">Ordered List</Button>
-            <Button type="button" value={"`code `"} onClick={handleButton} variant="contained" color="inherit">Code</Button>
-            <Button type="button" value={"---"} onClick={handleButton} variant="contained" color="inherit">Horizontal Rule</Button>
             <Button type="button" value={"[title](https://www.example.com)"} onClick={handleButton} variant="contained" color="inherit">Link</Button>
             <Button type="button" value={"![alt text](image.jpg)\n Please Note: Image will not show up on this doc. Please make a path to the image in your directory."} onClick={handleButton} variant="contained" color="inherit">Image</Button>
-            <h3>More Features / Coming Soon</h3>
+            <h3>Intermediate Formatting</h3>
+            <Button type="button" value={"---"} onClick={handleButton} variant="contained" color="inherit">Horizontal Rule</Button>
+            <Button type="button" value={"`code `"} onClick={handleButton} variant="contained" color="inherit">Code</Button>
+            <Button type="button" value={"[text][1]\n\n[1]: https://www.example.com"} onClick={handleButton} variant="contained" color="inherit">Reference Lists</Button>
+            <Button type="button" value={"\\ Text \\"} onClick={handleButton} variant="contained" color="inherit">Escape</Button>
+
+
         </div>
         <div className="viewer">
           <MarkdownViewer addedText={text}/>
